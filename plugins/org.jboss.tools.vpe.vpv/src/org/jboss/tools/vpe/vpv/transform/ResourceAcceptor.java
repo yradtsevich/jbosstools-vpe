406d996fc419302505d31f73b8525a484efa2911
Ilya Buziuk <ibuziuk@exadel.com>
2012-11-26 20:15:45 +0300

    Adding functionality of relative Root path handling

--- plugins/org.jboss.tools.vpe.vpv/src/org/jboss/tools/vpe/vpv/transform/ResourceAcceptor.java
+++ plugins/org.jboss.tools.vpe.vpv/src/org/jboss/tools/vpe/vpv/transform/ResourceAcceptor.java
@@ -2,11 +2,9 @@ package org.jboss.tools.vpe.vpv.transform;
 
 import java.io.File;
 
-import org.w3c.dom.Text;
-
 public interface ResourceAcceptor {
 
-	void acceptText(Text text, String mimeType);
+	void acceptText(String text, String mimeType);
 
 	void acceptFile(File file, String mimeType);
 
