fc4a2ad53f022d1421e11b2106f3f953a84f55ea
Yahor Radtsevich <yradtsevich@exadel.com>
2012-11-26 22:52:53 +0300

    Added VpvController initialization with VpvDomBuilder

--- plugins/org.jboss.tools.vpe.vpv/src/org/jboss/tools/vpe/vpv/transform/VpvController.java
+++ plugins/org.jboss.tools.vpe.vpv/src/org/jboss/tools/vpe/vpv/transform/VpvController.java
@@ -1,8 +1,14 @@
 package org.jboss.tools.vpe.vpv.transform;
 
 public class VpvController {
-	private VpvDomBuilder domBuilder;
+	private VpvDomBuilder domBuilder;	
 	
+	public VpvController(VpvDomBuilder domBuilder) {
+		this.domBuilder = domBuilder;
+	}
+
+
+
 	public static void getResource(String projectName, String path, Integer viewId, ResourceAcceptor resourceAcceptor) {		
 	}
 }
