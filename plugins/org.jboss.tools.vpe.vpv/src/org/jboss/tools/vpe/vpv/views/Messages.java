a454e4afd426795e230e457527a5991295e93db6
Ilya Buziuk <ibuziuk@exadel.com>
2012-12-10 16:35:14 +0300

    Removing unnecessary messages from property file

--- plugins/org.jboss.tools.vpe.vpv/src/org/jboss/tools/vpe/vpv/views/Messages.java
+++ plugins/org.jboss.tools.vpe.vpv/src/org/jboss/tools/vpe/vpv/views/Messages.java
@@ -6,8 +6,7 @@ public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "org.jboss.tools.vpe.vpv.views.messages"; //$NON-NLS-1$
 	public static String VpvView_REFRESH;
 	public static String VpvView_OPEN_IN_DEFAULT_BROWSER;
-	public static String VpvView_ERROR;
-	public static String VpvView_COULD_NOT_OPEN_DEFAULT_BROWSER;
+
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
