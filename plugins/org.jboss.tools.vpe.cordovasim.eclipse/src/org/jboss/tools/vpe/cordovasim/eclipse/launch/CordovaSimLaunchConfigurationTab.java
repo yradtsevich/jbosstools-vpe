af8f8d348c4b63116a14251efdf46cb40187ab43
Yahor Radtsevich <yahorr@gmail.com>
2013-04-15 01:35:09 +0300

    XXX: adding of cordovasim launch configuration in progress

--- plugins/org.jboss.tools.vpe.cordovasim.eclipse/src/org/jboss/tools/vpe/cordovasim/eclipse/launch/CordovaSimLaunchConfigurationTab.java
+++ plugins/org.jboss.tools.vpe.cordovasim.eclipse/src/org/jboss/tools/vpe/cordovasim/eclipse/launch/CordovaSimLaunchConfigurationTab.java
@@ -1,37 +1,64 @@
+/*******************************************************************************
+ * Copyright (c) 2007-2013 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
 package org.jboss.tools.vpe.cordovasim.eclipse.launch;
 
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.debug.internal.ui.SWTFactory;
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTab;
+import org.eclipse.debug.ui.WorkingDirectoryBlock;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
+/**
+ * @author "Yahor Radtsevich (yradtsevich)"
+ */
 public class CordovaSimLaunchConfigurationTab extends
 		AbstractLaunchConfigurationTab {
 
+	private Text rootFolderText;
+	private CordovaSimWorkingDirectoryBlock rootFolderBlock;
+	
+	public CordovaSimLaunchConfigurationTab() {
+		rootFolderBlock = new CordovaSimWorkingDirectoryBlock();
+	}
+
 	@Override
 	public void createControl(Composite parent) {
-		Composite comp = new Composite(parent, SWT.NONE);
-		setControl(comp);
+		Composite comp = SWTFactory.createComposite(parent, parent.getFont(), 1, 1, GridData.FILL_BOTH);
+		((GridLayout) comp.getLayout()).verticalSpacing = 0;
 		
-		GridLayout topLayout = new GridLayout();
-		topLayout.numColumns = 3;
-		comp.setLayout(topLayout);
+		Group group = SWTFactory.createGroup(comp, "Root Folder:", 2, 1, GridData.FILL_HORIZONTAL);
+		rootFolderText = SWTFactory.createSingleText(group, 1);
 		
 		Label fileLabel = new Label(comp, SWT.NONE);
 		fileLabel.setText("File"); //TODO
 		
-		Text fileText = new Text(parent, SWT.SINGLE | SWT.BORDER);
+		Text fileText = new Text(comp, SWT.SINGLE | SWT.BORDER);
 		fileText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		
 		Button fileButton = new Button(comp, SWT.PUSH);
 		fileButton.setText("Browse...");
 		
+		rootFolderBlock.createControl(comp);
+		
+		setControl(comp);		
 	}
 
 	@Override
@@ -54,8 +81,20 @@ public class CordovaSimLaunchConfigurationTab extends
 
 	@Override
 	public String getName() {
+		return "&Main";
+	}
+}
+
+class CordovaSimWorkingDirectoryBlock extends WorkingDirectoryBlock {
+
+	protected CordovaSimWorkingDirectoryBlock() {
+		super("org.jboss.tools.vpe.cordovasim.eclipse.launch.rootFolder");
+	}
+
+	@Override
+	protected IProject getProject(ILaunchConfiguration configuration)
+			throws CoreException {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 }
--- plugins/org.jboss.tools.vpe.cordovasim.eclipse/src/org/jboss/tools/vpe/cordovasim/eclipse/launch/CordovaSimLaunchConfigurationTabGroup.java
+++ plugins/org.jboss.tools.vpe.cordovasim.eclipse/src/org/jboss/tools/vpe/cordovasim/eclipse/launch/CordovaSimLaunchConfigurationTabGroup.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2007-2013 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
 package org.jboss.tools.vpe.cordovasim.eclipse.launch;
 
 import org.eclipse.debug.core.ILaunchConfiguration;
@@ -5,7 +15,11 @@ import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
 import org.eclipse.debug.ui.ILaunchConfigurationDialog;
 import org.eclipse.debug.ui.ILaunchConfigurationTab;
+import org.eclipse.debug.ui.CommonTab;
 
+/**
+ * @author "Yahor Radtsevich (yradtsevich)"
+ */
 public class CordovaSimLaunchConfigurationTabGroup extends
 		AbstractLaunchConfigurationTabGroup {
 
@@ -17,7 +31,8 @@ public class CordovaSimLaunchConfigurationTabGroup extends
 	@Override
 	public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
 		ILaunchConfigurationTab[] tabs = new ILaunchConfigurationTab[] {
-			new CordovaSimLaunchConfigurationTab()
+			new CordovaSimLaunchConfigurationTab(),
+			new CommonTab()
 		};
 		setTabs(tabs);
 	}
